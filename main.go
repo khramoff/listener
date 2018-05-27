db010c50b7c8f1a291167ead80ba4ffffeda4ebb
OleksiiKhramov <khramoff.al@gmail.com>
2018-05-27 21:31:23 +0300

    code refactoring minor bug fixes

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 	"github.com/stellar/go/clients/horizon"
+	"github.com/stellar/go/support/log"
 	"io/ioutil"
 	"net/http"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -20,11 +22,21 @@ var (
 
 const horizonAddress = "http://127.0.0.1:8000"
 const port = ":3000"
-const offerOperationId = 3
+
+var client = &horizon.Client{
+	HTTP: http.DefaultClient,
+	URL:  horizonAddress,
+}
 
 type pair struct {
 	Time  time.Time     `json:"time"`
 	Offer horizon.Offer `json:"offer"`
+}
+
+type Response struct {
+	Ledger  string `json:"ledger,omitempty"`
+	Records []pair `json:"records,omitempty"`
+	Errors  string `json:"errors,omitempty"`
 }
 
 type LedgerPage struct {
@@ -39,150 +51,140 @@ type LedgerPage struct {
 }
 
 func dbGetOffer(start, end string) []pair {
+	print("parsing time")
 	var result []pair
 
-	s, err := time.Parse(time.RFC3339, end)
+	s, err := time.Parse(time.RFC3339, start)
 	if err != nil {
-		println(err)
+		log.Error(err)
 	}
-	e, err := time.Parse(time.RFC3339, start)
+	e, err := time.Parse(time.RFC3339, end)
 	if err != nil {
-		println(err)
+		log.Error(err)
 	}
 
 	mutex.Lock()
 	for _, v := range memory {
-		if s.After(v.Time) && e.Before(v.Time) {
+		if s.Before(v.Time) && e.After(v.Time) {
 			result = append(result, v)
 		}
 	}
 	mutex.Unlock()
-
 	return result
-}
-
-func offerCtx(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var offers []pair
-
-		if start, end := chi.URLParam(r, "start"), chi.URLParam(r, "end"); start != "" && end != "" {
-			offers = dbGetOffer(start, end)
-		} else {
-			js, err := json.Marshal([]byte("Not found"))
-
-			if err != nil {
-				println(err)
-			}
-
-			w.Write(js)
-			return
-		}
-		ctx := context.WithValue(r.Context(), "offers", offers)
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
-}
-
-func GetOffer(w http.ResponseWriter, r *http.Request) {
-	offer := r.Context().Value("offers").([]pair)
-	js, err := json.Marshal(offer)
-	if err != nil {
-		panic(err)
-	}
-	w.Write(js)
 }
 
 func getCursor(client *horizon.Client) string {
 	resp, err := client.HTTP.Get(client.URL + "/ledgers?limit=1")
 	if err != nil {
-		panic(err)
+		log.Error(err)
 	}
 	buffer, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		log.Error(err)
 	}
 	var page LedgerPage
 	err = json.Unmarshal(buffer, &page)
 	if err != nil {
-		panic(err)
+		log.Error(err)
 	}
 
 	return page.Embedded.Records[0].PT
 }
 
-func fetchLedgers() {
-
-	client := &horizon.Client{
-		HTTP: http.DefaultClient,
-		URL:  horizonAddress,
+func offersExist(offer horizon.Offer) bool {
+	if offer.Seller != "" && offer.Price != "" {
+		return true
+	} else {
+		return false
 	}
+}
+
+func addOffers(buffer []byte, closed time.Time) {
+	var offersPage horizon.OffersPage
+
+	err := json.Unmarshal(buffer, &offersPage)
+	if err != nil {
+		log.Error(err)
+	}
+	mutex.Lock()
+	for _, offer := range offersPage.Embedded.Records {
+		if offersExist(offer) {
+			memory = append(memory, pair{Time: closed, Offer: offer})
+		} else {
+			continue
+		}
+	}
+	mutex.Unlock()
+}
+
+func operationHandler(link string, closed time.Time) {
+	resp, err := client.HTTP.Get(link)
+	if err != nil {
+		log.Error(err)
+	}
+	buffer, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error(err)
+	}
+
+	addOffers(buffer, closed)
+
+}
+
+func ledgerHandler(ledger horizon.Ledger) {
+	mutex.Lock()
+	sequenceNumber = ledger.Sequence
+	mutex.Unlock()
+
+	if ledger.OperationCount != 0 {
+		operationHandler(ledger.Links.Operations.Href, ledger.ClosedAt)
+	} else {
+		return
+	}
+}
+
+func fetchLedgers() {
 
 	pagingToken := getCursor(client)
 
 	cur := horizon.Cursor(pagingToken)
 
-	client.StreamLedgers(context.TODO(),
-		&cur,
-		func(ledger horizon.Ledger) {
-			mutex.Lock()
-			sequenceNumber = ledger.Sequence
-			mutex.Unlock()
-
-			if ledger.OperationCount != 0 {
-				resp, err := client.HTTP.Get(ledger.Links.Operations.Href)
-				if err != nil {
-					println(err)
-				}
-				buffer, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					println(err)
-				}
-
-				var op horizon.OffersPage
-				var OpType struct {
-					TypeI int `json:"type_i"`
-				}
-				err = json.Unmarshal(buffer, OpType)
-
-				if err != nil {
-					println(err)
-				}
-
-				if OpType.TypeI != offerOperationId {
-					return
-				}
-
-				err = json.Unmarshal(buffer, &op)
-				if err != nil {
-					println(err)
-				}
-				mutex.Lock()
-				for _, offer := range op.Embedded.Records {
-					memory = append(memory, pair{Time: ledger.ClosedAt, Offer: offer})
-				}
-				mutex.Unlock()
-			} else {
-				return
-			}
-		})
+	client.StreamLedgers(context.TODO(), &cur, ledgerHandler)
 }
 
 func main() {
 	go fetchLedgers()
 
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+	router.Use(render.SetContentType(render.ContentTypeJSON))
+
 	router.Get("/ledger", func(w http.ResponseWriter, r *http.Request) {
-		js, err := json.Marshal([]byte(strconv.FormatInt(int64(sequenceNumber), 10)))
+		var js Response
+		js.Ledger = string(sequenceNumber)
+		resp, err := json.Marshal(js)
 
 		if err != nil {
-			println(err)
+			log.Error(err)
 		}
 
-		w.Write(js)
+		w.Write(resp)
 	})
 
-	router.Get("/offers{?start,end}", func(w http.ResponseWriter, r *http.Request) {
-		router.With(offerCtx)
-		router.Get("/", GetOffer)
+	router.Get("/offers", func(w http.ResponseWriter, r *http.Request) {
+		start, end := r.URL.Query().Get("start"), r.URL.Query().Get("end")
+		var js Response
+		js.Records = dbGetOffer(start, end)
+		if len(js.Records) == 0 {
+			js.Errors = "404: Not found"
+			w.WriteHeader(404)
+		}
+		resp, err := json.Marshal(js)
+		if err != nil {
+			log.Error(err)
+		}
+		w.Write(resp)
 	})
 
 	http.ListenAndServe(port, router)
